8c3fafefb3f29de1e4754efbc095090232fd098d
EsmaeeilMoradi <esmaeeilmoradi700@gmail.com>
2023-08-05 11:25:38 +0330

    Defining Variables.

--- app/src/main/java/com/esm/ocsalyacademyandroid13/MainActivity.kt
+++ app/src/main/java/com/esm/ocsalyacademyandroid13/MainActivity.kt
@@ -7,5 +7,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val foundedYearOfOcsaly = 2019
+        val pi: Float = 3.14f
+        var currentYear = 2024
+        var isItRaining: Boolean = true
+        val KotlinIsBest: Boolean = true
+        val isItKotlin = false
+        val academyName: String = "OCSALY Academy"
+        var lecturerName: String = "Typhon .M"
+
+        //VAR variabels can be reassigned
+        currentYear = 2025
+        isItRaining = false
+        lecturerName = "John .J"
+
     }
 }
