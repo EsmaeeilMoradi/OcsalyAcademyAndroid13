3da9cb26cb1510d1e1c017f34c86bd01a3dd18e9
EsmaeeilMoradi <esmaeeilmoradi700@gmail.com>
2023-08-05 11:28:06 +0330

    Kotlin Starting.
    Assignment, Addition and Substraction operators, Division, Multiplaction, Increment and Decrement operators in Kotlin.

--- app/src/main/java/com/esm/ocsalyacademyandroid13/MainActivity.kt
+++ app/src/main/java/com/esm/ocsalyacademyandroid13/MainActivity.kt
@@ -2,6 +2,7 @@ package com.esm.ocsalyacademyandroid13
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,19 +10,55 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val foundedYearOfOcsaly = 2019
-        val pi: Float = 3.14f
-        var currentYear = 2024
-        var isItRaining: Boolean = true
-        val KotlinIsBest: Boolean = true
-        val isItKotlin = false
-        val academyName: String = "OCSALY Academy"
-        var lecturerName: String = "Typhon .M"
+        val myTextView: TextView = findViewById(R.id.myText)
+        val foundedYearOfOcsaly = 2019 // ASSIGNMENT OPERATOR
+        val pi: Float = 3.14f // ASSIGNMENT OPERATOR
+        var currentYear = 2024 // ASSIGNMENT OPERATOR
+        var isItRaining: Boolean = true // ASSIGNMENT OPERATOR
+        val KotlinIsBest: Boolean = true // ASSIGNMENT OPERATOR
+        val isItKotlin = false // ASSIGNMENT OPERATOR
+        val academyName: String = "OCSALY Academy" // ASSIGNMENT OPERATOR
+        var lecturerName: String = "Typhon .M" // ASSIGNMENT OPERATOR
 
         //VAR variabels can be reassigned
-        currentYear = 2025
-        isItRaining = false
-        lecturerName = "John .J"
+        currentYear = 2025 // ASSIGNMENT OPERATOR
+        isItRaining = false // ASSIGNMENT OPERATOR
+        lecturerName = "John .J" // ASSIGNMENT OPERATOR
+
+        var ourProgrammingLanguage = "Kotlin" // ASSIGNMENT OPERATOR
+        //ourProgrammingLanguage = 11 // ASSIGNMENT OPERATOR
+
+        lecturerName = academyName // ASSIGNMENT OPERATOR
+
+        myTextView.text = lecturerName
+
+
+
+
+
+
+        var ageOfOcsaly = currentYear - foundedYearOfOcsaly
+
+
+
+
+        ageOfOcsaly = ageOfOcsaly * ageOfOcsaly // MULTIPLICATION OPERATOR
+
+        myTextView.text = ""+(4 * 4)  // MULTIPLICATION OPERATOR
+
+        ageOfOcsaly = ageOfOcsaly / ageOfOcsaly // DIVISION OPERATOR
+        myTextView.text = "" + (20 / 4)  // DIVISION OPERATOR
+
+
+        var myNumberToIncrement = 5
+        myNumberToIncrement++
+        ageOfOcsaly = ageOfOcsaly * ageOfOcsaly // INCREMENT OPERATOR
+        myTextView.text = "$myNumberToIncrement" // INCREMENT OPERATOR
+
+        myNumberToIncrement = 5
+        myNumberToIncrement--
+        ageOfOcsaly = ageOfOcsaly * ageOfOcsaly // INCREMENT OPERATOR
+        myTextView.text = "$myNumberToIncrement" // INCREMENT OPERATOR
 
     }
 }
